6d38cf7e1a1be02740f89f127615a89e18373ac4
Harmeet Singh <hskhanduja03@gmail.com>
2025-07-31 13:43:26 +0530

    first commit

--- index.ts
+++ index.ts
@@ -14,7 +14,9 @@ const PORT = 5000;
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
-
+app.get("/test", (req, res) => {
+    res.send("working")
+});
 app.post("/api/generate", async (req, res) => {
   const { subject, level } = req.body;
 
